b489f730ecdba90405b49ed19e3b44f420aba718
Jesús E. Castelán <castelan_jesus@hotmail.com>
2022-11-09 12:08:01 -0600

    minor changes

--- surfshark_generator.sh
+++ surfshark_generator.sh
@@ -1,66 +1,75 @@
-  DIR="$HOME/conf_files"
-
-echo "Enter your surfshark's private key"
-read pk
-
-download_conf_files(){
+DIR="$HOME/WG_Surfshark/conf_files"
 
 if [ -d "$DIR" ]; then
   echo "$DIR exists"
 else
-echo "Crating $DIR"
-  mkdir $DIR
+  echo "$DIR now created"
+  mkdir -p $DIR
 fi
-wget -i $PWD/url/urls -P $DIR
+
+
+	if [ "$(ls -A "$DIR")" ]; then
+     echo "$DIR is populated with the next files:"
+     ls $DIR
+	else
+    echo "$DIR is empty. Your .conf files will be downloaded here or if you already have your .conf files, you can place them here to change your private key."
+    fi
+	
+
+download_conf_files() {
+  rm $DIR/*.conf 
+  wget -i $PWD/url/urls -P $DIR
 }
 
 insert_private_key() {
+  echo "Enter your surfshark's private key"
+  read pk
   cd $DIR
   for i in *; do
-
-sed -i "s|PrivateKey =.*|PrivateKey = $pk|g" $i
-
-done
+    sed -i "s|PrivateKey =.*|PrivateKey = $pk|g" $i
+  done
 }
 
-rename_files(){
-    cd $DIR
-
-for i in *; do
-
-  final_name=$(find "$i"| grep -h -o -P '.{0,7}prod.{0,0}')
+rename_files() {
+  cd $DIR
+  for i in *; do
+    final_name=$(find "$i" | grep -h -o -P '.{0,7}prod.{0,0}')
     mv $i $final_name.conf
-    done
+  done
 }
 
-compress_conf_files(){
-cd $DIR
-tar -cvf conf_files.tar -C $PWD *.conf
-tar -czf conf_files.tar.gz -C $PWD *.conf
-zip conf_files.zip *.conf
+compress_conf_files() {
+  cd $DIR
+  tar -cvf $(dirname "$DIR")/conf_files.tar -C $DIR *.conf
+  tar -czf $(dirname "$DIR")/conf_files.tar.gz -C $DIR *.conf
+  zip $(dirname "$DIR")/conf_files.zip *.conf
 }
 
-echo "Select an option"
+echo -e "
+1) Download blank .conf files from SurfShark servers, insert your private key and then generate .tar, .tar.gz and .zip (compatible with most routers and official WireGuard apps). Will be saved in $DIR 
+2) Update existing .conf files located in $DIR to change existing private key
+3) Only generate .tar, .tar.gz and .zip from files in $DIR. (Will be saved in $(dirname "$DIR"))"
 
-select option in generate_wg_config_files update_config_files compress_conf_files_tar_zip; do
+echo -e "\e[1;31mSelect an option:"
+read opt
+  case $opt in
+  1)
+    download_conf_files
+    insert_private_key
+    rename_files
+    compress_conf_files
+    ;;
 
+  2)
+    insert_private_key
+    ;;
 
-    case $option in
-    "generate_wg_config_files")
-        download_conf_files
-        insert_private_key
-        rename_files
-        compress_conf_files
-        ;;
+  3)
+    compress_conf_files
+    ;;
 
-    "update_config_files")
-        insert_private_key
-        ;;
+    *)
+    echo "Not a valid option"
+    ;;
 
-        "compress_conf_files_tar_zip")
-        compress_conf_files
-        ;;
-
-
-    esac
-done
+  esac
